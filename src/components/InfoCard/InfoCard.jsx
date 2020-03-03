e345c4e4ed5c1beeb9f770da9acbcf601bf66e22
JJ Gray <JJ.Gray@Nology.local>
2020-03-03 16:02:35 +0000

    changed spacing and flex of component - awaiting assets / variables

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -4,8 +4,10 @@ import styles from "./InfoCard.module.scss";
 const InfoCard = () => {
   return (
     <section className={styles.card}>
-      <h3>Your magnesium needs:</h3>
-      <h2>High</h2>
+      <div>
+        <h3>Your magnesium needs:</h3>
+        <h2>High</h2>
+      </div>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit debitis illum esse, fugiat
         quasi magnam vitae placeat laudantium delectus nesciunt doloremque laboriosam totam nobis
