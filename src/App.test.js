7e4d3a5555c783a8cc443860b363b4cb5405aa9b
James Panter <James.Panter@Nology.local>
2020-03-03 17:24:33 +0000

    test travis commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+// test
 
 test("renders learn react link", () => {
   expect(render(<App />)).toBeTruthy();
