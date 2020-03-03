4baeaed71842c61d769b3fd26cdf8c1b41c48aa3
Denis Denchev <Denis.Denchev@Nology.local>
2020-03-03 16:41:19 +0000

    navBar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className = "NavBar">
+        <NavBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./NavBar.module.scss";
+
+const NavBar = () => {
+  return (
+    <>
+      <section className={styles.navBar}>
+        <img src = "prompt" alt = "Logo"></img>
+        <section className={styles.links}>
+          <p>Priorities</p>
+          <p>Nutrients</p>
+          <p>Diet</p>
+          <p>Health</p>
+          <p>Genetics</p>
+          <p>Answers</p>
+          <p>Settings</p>
+          <p className={styles.LogOut}>Log out</p>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
