26672aa1a816364e73a9e2c2c2fa8dcf67d70bda
James Panter <James.Panter@Nology.local>
2020-03-05 15:33:45 +0000

    Removed comments and unused import

--- src/components/CheckBox/CheckBox.jsx
+++ src/components/CheckBox/CheckBox.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./CheckBox.module.scss";
-import Tick from "../../assets/graphic-devices/black-straw.svg";
 
 const CheckBox = props => {
   const { text } = props;
   return (
     <label className={styles.container}>
-      <input type="checkbox" />
+      <input type="checkbox" name="checkAddress" />
       {text}
       <span className={styles.checkmark}></span>
     </label>
--- src/components/CheckBox/CheckBox.test.js
+++ src/components/CheckBox/CheckBox.test.js
@@ -1,10 +1,8 @@
 import React from "react";
 import CheckBox from "./CheckBox";
-
+import { render } from "@testing-library/react";
 describe("CheckBox tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
-  })
+  it("should render", () => {
+    expect(render(<CheckBox />)).toBeTruthy();
+  });
 });
