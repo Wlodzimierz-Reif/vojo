4a1332d578318a798e32a5885d88328ad59bf992
peter oboyle <peter.oboyle@Nology.local>
2020-03-05 17:20:38 +0000

    test removed from app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import styles from "./App.module.scss";
-// import MaintenancePage from "./components/MaintenancePage";
-import PriorityBox from "./components/PriorityBox";
+import MaintenancePage from "./components/MaintenancePage";
 
 const App = () => {
-  const testObj = {
-    priorityBoxNum: 1,
-    priorityBoxHeading: "hello",
-    priorityBoxLink: "https://github.com/",
-    priorityBoxLinkText: "hello"
-  };
-  return (
-    <div className={styles.maintenanceContainer}>
-      {/* <MaintenancePage /> */}
-      <PriorityBox props={testObj} />
-    </div>
-  );
+  return <MaintenancePage />;
 };
 
 export default App;
