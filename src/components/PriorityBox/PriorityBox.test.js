fe83edac568bb208a5c2507f8cd872ecf95c5c6e
peter oboyle <peter.oboyle@Nology.local>
2020-03-05 16:28:01 +0000

    added test

--- src/components/PriorityBox/PriorityBox.test.js
+++ src/components/PriorityBox/PriorityBox.test.js
@@ -2,9 +2,7 @@ import React from "react";
 import PriorityBox from "./PriorityBox";
 
 describe("PriorityBox tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
-  })
+  it("should render", () => {
+    expect(render(<PriorityBox />)).toBeTruthy();
+  });
 });
