e1251d44ddcf376100adafb8706280d1c839f5a1
JJ Gray <JJ.Gray@Nology.local>
2020-03-06 09:30:20 +0000

    added checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import styles from "./App.module.scss";
 // import MaintenancePage from "./components/MaintenancePage";
 import Button from "./components/Button";
+import CheckBox from "./components/CheckBox";
 
 const App = () => {
   return (
     <>
-      <div className={styles.buttonContainer}>
-        <Button btnText="Button" />
-      </div>
+      <section className="button">
+        <h2>Button Component</h2>
+        <div className={styles.buttonContainer}>
+          <Button btnText="Button" />
+        </div>
+      </section>
+
+      <section className="CheckBox">
+        <h2>CheckBox Component</h2>
+        <div className={styles.checkBoxContainer}>
+          <CheckBox text="Default off" startChecked={false} />
+          <CheckBox text="Default on" startChecked={true} />
+        </div>
+      </section>
     </>
     // <div className={styles.maintenanceContainer}>
     //   <MaintenancePage />
