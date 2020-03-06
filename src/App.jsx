8a1b225e43b15ef4c09157c1631927fff72fa63f
JJ Gray <JJ.Gray@Nology.local>
2020-03-06 10:08:09 +0000

    Added sliders

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,39 @@ import styles from "./App.module.scss";
 // import MaintenancePage from "./components/MaintenancePage";
 import Button from "./components/Button";
 import CheckBox from "./components/CheckBox";
+import DisplaySlider from "./components/DisplaySlider";
 
 const App = () => {
   return (
     <>
-      <section className="button">
-        <h2>Button Component</h2>
-        <div className={styles.buttonContainer}>
-          <Button btnText="Button" />
-        </div>
-      </section>
+      <section className={styles.topFlex}>
+        <article className={styles.button}>
+          <h2>Button Component</h2>
+          <div className={styles.buttonContainer}>
+            <Button btnText="Button" />
+          </div>
+        </article>
 
-      <section className="CheckBox">
-        <h2>CheckBox Component</h2>
-        <div className={styles.checkBoxContainer}>
-          <CheckBox text="Default off" startChecked={false} />
-          <CheckBox text="Default on" startChecked={true} />
-        </div>
+        <article className={styles.checkBox}>
+          <h2>CheckBox Component</h2>
+          <div className={styles.checkBoxContainer}>
+            <CheckBox text="Default off" startChecked={false} />
+            <CheckBox text="Default on" startChecked={true} />
+          </div>
+        </article>
+
+        <article className={styles.slider}>
+          <h2>Slider Component</h2>
+          <div className={styles.sliderContainer}>
+            <DisplaySlider sliderColor="greenToRed" />
+          </div>
+          <div className={styles.sliderContainer}>
+            <DisplaySlider sliderColor="redGreenYellow" />
+          </div>
+          <div className={styles.sliderContainer}>
+            <DisplaySlider sliderColor="redToGreen" />
+          </div>
+        </article>
       </section>
     </>
     // <div className={styles.maintenanceContainer}>
