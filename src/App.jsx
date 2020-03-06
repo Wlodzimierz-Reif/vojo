109e938df43bfee9fc792635de8ff0213d82250e
JJ Gray <JJ.Gray@Nology.local>
2020-03-06 09:22:30 +0000

    Added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./App.module.scss";
-import MaintenancePage from "./components/MaintenancePage";
-
+// import MaintenancePage from "./components/MaintenancePage";
+import Button from "./components/Button";
 
 const App = () => {
   return (
-    <div className={styles.maintenanceContainer}>
-      <MaintenancePage />
-    </div>
+    <>
+      <div className={styles.buttonContainer}>
+        <Button btnText="Button" />
+      </div>
+    </>
+    // <div className={styles.maintenanceContainer}>
+    //   <MaintenancePage />
+    // </div>
   );
 };
 
