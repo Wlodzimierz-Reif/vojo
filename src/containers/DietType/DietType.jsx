75ceb8d3308546e90795dd91ae1cd3c69a121692
matenemeth1994 <mate.nemeth@nology.io>
2020-03-10 17:21:26 +0000

    final changes youe diet type first page

--- src/containers/DietType/DietType.jsx
+++ src/containers/DietType/DietType.jsx
@@ -5,9 +5,8 @@ import Image2 from "../../assets/characters/broccoli-1.svg";
 import Image from "../../assets/characters/bean-2.svg";
 
 const DietType = props => {
-  const foodArray = ["Beans", "Lentils", "Greens (peas, spinach, etc.)"];
-  
   const { regionText, dietText } = props;
+  
   return (
     <>
       {/* <h2>Diet</h2> */}
