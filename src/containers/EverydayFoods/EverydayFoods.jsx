3a092949085ec3e453d11f49bfb6a711a5c9c422
Bogdan Niculescu <Bogdan.Niculescu@Nology.local>
2020-03-10 18:10:04 +0000

    everyday foods ticket

--- src/containers/EverydayFoods/EverydayFoods.jsx
+++ src/containers/EverydayFoods/EverydayFoods.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./EverydayFoods.module.scss";
+import InfoList from "../../components/InfoList";
 
 const EverydayFoods = () => {
   return (
     <>
-      <section className={styles.dietContainer}></section>
-      <h3 className={styles.title}>Your go-to everyday foods</h3>
+      <section className={styles.dietContainer}>
+        <h3 className={styles.title}>Your go-to everyday foods</h3>
+        <InfoList />
+      </section>
     </>
   );
 };
