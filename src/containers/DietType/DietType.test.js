48b1a5fe019c2130c602e712092183d59508774a
matenemeth1994 <mate.nemeth@nology.io>
2020-03-11 14:16:54 +0000

    fixed test import render

--- src/containers/DietType/DietType.test.js
+++ src/containers/DietType/DietType.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DietType from "./DietType";
-
+import { render } from "@testing-library/react";
+ 
 describe("DietType tests", () => {
   it("should render", () => {
     expect(render(<DietType />)).toBeTruthy();
