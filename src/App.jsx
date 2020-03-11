eb5b8e6acbf7ebb4b20b3d263d1f683dcef3d0c6
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-03-11 15:09:45 +0000

    do-not-push

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 // import styles from "./App.module.scss";
 import Router from "../src/containers/Routes";
 // import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Router />
+      <Footer />
     </>
   );
 };
