bc82f2906c2cac09b5cc8951420a04eea1aed633
James Nowecki <James.Nowecki@Nology.local>
2020-03-11 15:45:24 +0000

    Links to pages added

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./HomePage.module.scss";
 import MaintenancePage from "../../components/MaintenancePage";
+import { Link } from "@reach/router";
 
 const HomePage = () => {
   return (
-    <div className={styles.maintenanceContainer}>
-      <MaintenancePage />
+    <div>
+      <Link to="supplements-page">Supplements Page</Link>
+      <Link to="priorities-page">Priorities Page</Link>
+
+      <div className={styles.maintenanceContainer}>
+        <MaintenancePage />
+      </div>
     </div>
   );
 };
