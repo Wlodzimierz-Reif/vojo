7c3c0619a1837744866e035e1f6331aa349a31ca
Bogdan Niculescu <Bogdan.Niculescu@Nology.local>
2020-03-12 16:49:21 +0000

    vojo project

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import styles from "./App.module.scss";
 import MealIdeasTop from "./components/MealIdeasTop";
 import MealIdeasBottom from "./components/MealIdeasBottom/MealIdeasBottom";
+import EverdayFoods from "./containers/EverydayFoods";
+import EverydayFoods from "./containers/EverydayFoods";
+import InfoList from "./components/InfoList";
+import Image from "./assets/characters/characters-together.svg";
 
 const App = () => {
   return (
     <>
-      <section className={styles.mealIdeas}>
+      <section className={styles.infoSection}>
+        <EverydayFoods />
+      </section>
+      {/* <section className={styles.mealIdeas}>
         <div className={styles.dietContainer}>
           <MealIdeasTop />
           <MealIdeasBottom />
@@ -31,7 +38,7 @@ const App = () => {
           <MealIdeasBottom />
           <MealIdeasBottom />
         </div>
-      </section>
+  </section> */}
     </>
   );
 };
