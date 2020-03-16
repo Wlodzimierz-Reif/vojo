7e9821680fd79887a00c807590c0465bb74e9b1b
Lucian Iernye <Lucian.Iernye@Nology.local>
2020-03-16 18:49:27 +0000

    some input fields added

--- src/components/PaymentDetails/PaymentDetails.jsx
+++ src/components/PaymentDetails/PaymentDetails.jsx
@@ -7,25 +7,26 @@ const PaymentDetails = () => {
   return (
     <>
       <section>
-        <div className={styles.paymentDetails}>
+        <div>
           <h3>Payment Details</h3>
           <InputLabel isPrimary={true} labelName="Your Information" />
           <InputField type="text" name="full name" placeholder="Full Name" />
           <InputField type="email" name="email" placeholder="Email" />
         </div>
 
-        <div className={styles.paymentDetails}>
+        <div>
           <InputLabel isPrimary={true} labelName="Card Details" />
           <InputField
             type="text"
             name="card number"
             placeholder="Card Number"
           />
-
-          <div className={styles.smallBtn}>
-            <InputField type="text" name="CVV" placeholder="CVV" />
-            <InputField type="text" name="CVV" placeholder="MM / YY" />
-            <InputField type="text" name="CVV" placeholder="Postal Code" />
+          <div>
+            <div>
+              <InputField type="text" name="CVV" placeholder="CVV" />
+              <InputField type="text" name="CVV" placeholder="MM / YY" />
+              <InputField type="text" name="CVV" placeholder="Postal Code" />
+            </div>
           </div>
         </div>
       </section>
