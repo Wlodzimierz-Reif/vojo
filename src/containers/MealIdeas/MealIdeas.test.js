d53d29252aced19abdae723a42a31a4ca268de1a
Bogdan Niculescu <Bogdan.Niculescu@Nology.local>
2020-03-17 18:24:17 +0000

    test added

--- src/containers/MealIdeas/MealIdeas.test.js
+++ src/containers/MealIdeas/MealIdeas.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 import MealIdeas from "./MealIdeas";
+import { render } from "@testing-library/react";
 
-describe("MealIdeas tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
-  })
+describe("MealIdeas test", () => {
+  it("should render", () => {
+    expect(render(<MealIdeas />)).toBeTruthy();
+  });
 });
