70639c5f47794b9db189d4a9ef1a7f09e3e55d10
Denis Denchev <Denis.Denchev@Nology.local>
2020-03-18 17:20:41 +0000

    test written for inputfield

--- src/components/InputField/InputField.test.js
+++ src/components/InputField/InputField.test.js
@@ -1,9 +1,34 @@
 import React from "react";
 import InputField from "./InputField";
 import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 
 describe("InputField tests", () => {
+  let testFunc;
+  let component;
+
+  beforeEach(() => {
+    testFunc = jest.fn();
+    component = shallow(
+      <InputField
+        type="testInput"
+        name="testName"
+        placeholder="testText"
+        handleInput={testFunc}
+      />
+    );
+  });
+
   it("should render", () => {
-    expect(render(<InputField />)).toBeTruthy();
+    expect(render(component)).toBeTruthy();
+  });
+
+  it("should render the correct placeholder text", () => {
+    expect(
+      component
+        .find("input")
+        .at(0)
+        .props().placeholder
+    ).toEqual("testText");
   });
 });
