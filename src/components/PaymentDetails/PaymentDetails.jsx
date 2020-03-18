b17073eb1c78d7c2dc38b258e55cdb5ac54773e1
Lucian Iernye <lucian.iernye@gmail.com>
2020-03-18 21:24:34 +0000

    media querries added for the payment form

--- src/components/PaymentDetails/PaymentDetails.jsx
+++ src/components/PaymentDetails/PaymentDetails.jsx
@@ -1,40 +1,42 @@
-import React from "react";
-import styles from "./PaymentDetails.module.scss";
-import InputLabel from "../InputLabel/InputLabel";
-import InputField from "../InputField/InputField";
+import React from 'react';
+import styles from './PaymentDetails.module.scss';
+import InputLabel from '../InputLabel/InputLabel';
+import InputField from '../InputField/InputField';
 
 const PaymentDetails = () => {
   return (
     <>
-      <section className={styles.inputContainer}>
-        <h3>Payment Details</h3>
+      <div className={styles.container}>
+
+        <div className={styles.header}>
+          <h5>Payment Details</h5>
+        </div>
+
         <div className={styles.info}>
-          <p>Your Information</p>
-          <div>
-            <InputField type="text" name="full name" placeholder="Full Name" />
+          <p>Your information</p>
+          <div className={styles.inputs}>
+            <InputField type="text" name="full name" placeholder="Full name" />
           </div>
-          <div>
+          <div className={styles.inputs}>
             <InputField type="email" name="email" placeholder="Email" />
           </div>
         </div>
 
-        <div className={styles.cardContainer}>
+        <div className={styles.info}>
           <p>Card Details</p>
-          <div>
-            <InputField
-              type="text"
-              name="card number"
-              placeholder="Card Number"
-              className={styles.input}
-            />
+          <div className={styles.inputs}>
+            <InputField type="text" name="card number" placeholder="Card number" />
           </div>
-          <div className={styles.cardInfo}>
+          <div className={styles.smallInputs}>
             <InputField type="text" name="CVV" placeholder="CVV" />
-            <InputField type="text" name="CVV" placeholder="MM / YY" />
-            <InputField type="text" name="CVV" placeholder="Postal Code" />
+            <InputField type="text" name="expire date" placeholder="MM / YY" />
+            <InputField type="text" name="postal code" placeholder="Postal Code" />
           </div>
         </div>
-      </section>
+        
+      </div>
+
+
     </>
   );
 };
--- src/containers/PaymentPage/PaymentPage.jsx
+++ src/containers/PaymentPage/PaymentPage.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import styles from "./PaymentPage.module.scss";
-import PaymentDetails from "../../components/PaymentDetails/PaymentDetails";
-import OrderSummary from "../../components/OrderSummary/OrderSummary";
-import PayButton from "../../components/PayButton/PayButton";
+import React from 'react';
+import styles from './PaymentPage.module.scss';
+import PaymentDetails from '../../components/PaymentDetails/PaymentDetails';
+import OrderSummary from '../../components/OrderSummary/OrderSummary';
+import PayButton from '../../components/PayButton/PayButton';
 
 const PaymentPage = () => {
-  return (
+	return (
     <>
-      <h1>Review & Complete Your Order</h1>
-      <section>
-        <div className={styles.paymentDetails}>
-          <PaymentDetails />
-        </div>
-        <div className={styles.orderSummary}>
-          <OrderSummary />
-          <PayButton btnText={"Pay with Paypal"} isPaypal={true} />
-          <PayButton btnText={"Pay Now"} isPaypal={false} />
-        </div>
-      </section>
+    <div className={styles.header}>
+      <h2>Review & Complete Your Order</h2>
+    </div>
+		<section className={styles.mainContainer}>
+			<div className={styles.details}>
+        <PaymentDetails />
+      </div>
+			<div className={styles.summary}>
+        <OrderSummary title="Order Summary" nrOfItems="1" nameOfItem="DNA kit" totalPrice="£100"/>
+      </div>
+		</section>
     </>
-  );
+	);
 };
 
 export default PaymentPage;
