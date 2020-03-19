e607ac207a0a26acf06ca7067257b1908915d056
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-03-19 14:36:53 +0000

    testing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./App.module.scss";
 import Routes from "./containers/Routes";
-import RegisterDNA from "./containers/RegisterDNA/RegisterDNA";
 
 const App = () => {
   return (
-    // <>
-    //   <Routes />
-    // </>
-
-<>
-<RegisterDNA />
-</>
+    <>
+      <Routes />
+    </>
   );
 };
 
