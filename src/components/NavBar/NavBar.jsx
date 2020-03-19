2203eda96c5f38c38747a484e652f903018ccabc
oguru <oguru@hotmail.co.uk>
2020-03-19 14:24:16 +0000

    added some functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./NavBar.module.scss";
-import logos from "../../assets/logos/tofu-logo.png";
+import logos from "../../assets/logos/black-logo.png";
 
 const mainLinks = [
   { name: "Dashboard" },
@@ -16,15 +16,38 @@ const getLinks = link => {
   return <p> {link.name} </p>;
 };
 
-const NavBar = () => {
+// const currentPage = "Dashboard";
+
+const NavBar = (props) => {
+
+  // const { currentPage } = props;
+  const [navVisibility, toggleOpen] = useState(false);
+
+  let switchBurgerIcon = navVisibility === true ? styles.burgerMenuCross : styles.burgerMenuIcon;
+
+
   return (
-    <section className={styles.navBar}>
-      <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
+    <>
+    <div className={styles.navBar}>
+      <div className={styles.logoBurgerFlex}>
+        <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
+        {/* <p>{currentPage}</p> */}
+        <button onClick={() => toggleOpen(!navVisibility)} 
+        className={`${styles.burgerMenu} ${switchBurgerIcon}`}></button>
+      </div>     
       <section className={styles.links}>
+        <span className={styles.navLine}></span>
         {mainLinks.map(getLinks)}
-        <p className={styles.logOut}>Log out</p>
+        <span className={styles.navLine}></span>
       </section>
-    </section>
+      <section className={styles.navBottom}>
+        <p>Ask a question</p>
+        <p>T&Cs</p>
+        <p>Legal stuff</p>
+      </section>
+    </div>
+    </>
+    
   );
 };
 
