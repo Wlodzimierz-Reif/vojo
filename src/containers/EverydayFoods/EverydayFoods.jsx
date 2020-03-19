616d100f76a62eb49ab181d9806b5b332def2c83
Charlie Caseley <Charlie.Caseley@Nology.local>
2020-03-19 16:11:01 +0000

    commit merge

--- src/containers/EverydayFoods/EverydayFoods.jsx
+++ src/containers/EverydayFoods/EverydayFoods.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import styles from "./EverydayFoods.module.scss";
+import styles from "../EverydayFoods/EverydayFoods.module.scss";
+import InfoList from "../../components/InfoList";
+import Image from "../../assets/characters/characters-together.svg";
 
 const EverydayFoods = () => {
   return (
     <>
-      <section className={styles.dietContainer}></section>
-      <h3 className={styles.title}>Your go-to everyday foods</h3>
+      <div className={styles.listImage}>
+        <section className={styles.dietContainer}>
+          <h3 className={styles.title}>Your go-to everyday foods</h3>
+          <InfoList />
+          <InfoList />
+          <InfoList />
+          <InfoList />
+          <InfoList />
+        </section>
+        <img src={Image} alt="characters-together" />
+      </div>
     </>
   );
 };
