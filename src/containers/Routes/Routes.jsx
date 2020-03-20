0a475951600c013ea63c4e43eb8b6e696b1d9a3f
Charlie Caseley <Charlie.Caseley@Nology.local>
2020-03-20 14:35:27 +0000

    sorted merge conflict

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -5,34 +5,6 @@ import HomePage from "../HomePage";
 // import NutrientsPage from "../NutrientsPage";
 import NotFound from "../NotFound";
 import PrioritiesPage from "../PrioritiesPage";
-<<<<<<< HEAD
-import Footer from "../../components/Footer";
-import PaymentPage from "../PaymentPage/PaymentPage";
-import mockData from "../../data";
-import RegisterDNA from "../RegisterDNA";
-
-import DietBreakdown from "../DietBreakdown";
-
-const Routes = () => {
-  // Any object, any key that contains the words
-  // "recommendation" or "action" place into
-  // new recommendations array
-
-  return (
-    <>
-      <Router>
-        <Redirect noThrow from="/" to="priorities-page" />
-        <HomePage path="home-page" />
-        <NutrientsPage nutrients={mockData.nutrients} path="nutrients-page" />
-        <PrioritiesPage path="priorities-page" />
-        <PaymentPage path="payment-page" />
-        <RegisterDNA path="register-dna" />
-        <NotFound default />
-        <DietBreakdown brief={"ysfadud"} path="diet-breakdown" />
-      </Router>
-      <Footer />
-    </>
-=======
 import MealIdeas from "../MealIdeas/MealIdeas";
 import EverydayFoods from "../EverydayFoods/EverydayFoods";
 
@@ -46,7 +18,6 @@ const Routes = () => {
       <MealIdeas path="meal-ideas" />
       <NotFound default />
     </Router>
->>>>>>> 4db6e5083ec9cab80747936b943b31f8d15fdcf6
   );
 };
 
