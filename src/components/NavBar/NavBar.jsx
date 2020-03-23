08aa4be72710a59131af1a2785f8cb030e38080d
oguru <oguru@hotmail.co.uk>
2020-03-23 14:57:40 +0000

    animated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import styles from "./NavBar.module.scss";
 import logos from "../../assets/logos/black-logo.png";
+import Button from "../Button"
 
 const mainLinks = [
   { name: "Dashboard" },
   { name: "Diet plan" },
+  { name: "Health" },
   { name: "Nutrient breakdown" },
   { name: "Recipes" },
   { name: "Genetics" },
   { name: "Profile" },
-  { name: "Settings" }
 ];
 
 const getLinks = link => {
   return <p> {link.name} </p>;
 };
 
-// const currentPage = "Dashboard";
+const currentPage = "Dashboard";
 
 const NavBar = (props) => {
 
@@ -25,25 +26,42 @@ const NavBar = (props) => {
 
   let switchBurgerIcon = navVisibility === true ? styles.burgerMenuCross : styles.burgerMenuIcon;
 
+ 
+  const navOpen = {
+    height: { height: "calc(100vh - 32px)" },
+    showCurrentPage: {display: "none"}
+  }
+
+  const navClosed = {
+    height: { height: "56px", borderBottom: "solid 2px $primary-background"},
+    hide: {opacity: "0", transition: "0.2s"}
+  }
+  
+  let toggleNav = navVisibility === true ? navOpen : navClosed;
+
+  window.onresize = () => window.innerWidth >= 768 ? toggleOpen(true) : null;
 
   return (
     <>
-    <div className={styles.navBar}>
-      <div className={styles.logoBurgerFlex}>
-        <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
-        {/* <p>{currentPage}</p> */}
-        <button onClick={() => toggleOpen(!navVisibility)} 
-        className={`${styles.burgerMenu} ${switchBurgerIcon}`}></button>
-      </div>     
-      <section className={styles.links}>
-        <span className={styles.navLine}></span>
-        {mainLinks.map(getLinks)}
-        <span className={styles.navLine}></span>
+    <div className={`${styles.navBar} `} style={toggleNav.height}>
+      <section className={styles.navTop}>
+        <div className={styles.logoBurgerFlex}>
+          <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
+          <p style={toggleNav.showCurrentPage}>{currentPage}</p>
+          <button onClick={() => toggleOpen(!navVisibility)}
+          className={`${styles.burgerMenu} ${switchBurgerIcon}`}></button>
+        </div>
+        <div className={styles.links} style={toggleNav.hide}>
+          <p className={styles.navLine}></p>
+          {mainLinks.map(getLinks)}
+          <p className={styles.navLine}></p>
+        </div>
       </section>
-      <section className={styles.navBottom}>
+      <section className={styles.navBottom} style={toggleNav.hide}>
         <p>Ask a question</p>
         <p>T&Cs</p>
         <p>Legal stuff</p>
+        <Button btnText={"Join us on Facebook"} />
       </section>
     </div>
     </>
