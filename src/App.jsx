561cad394913288b48f48097c2ddfa063dcd9dc1
Wlodzimierz Reif <Wlodzimierz.Reif@Nology.local>
2020-03-23 17:39:49 +0000

    added progress bar and fixed alignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./App.module.scss";
 import Routes from "../src/containers/Routes";
+import ProgressBar from "./components/ProgressBar";
 
 const App = () => {
   return (
     <>
-      <ProgressBar value="50" />
+      {/* <ProgressBar value="50" /> */}
       <Routes />
     </>
   );
--- src/containers/QuestionnairePage/PageOne/PageOne.jsx
+++ src/containers/QuestionnairePage/PageOne/PageOne.jsx
@@ -7,6 +7,7 @@ import CheckBox from "../../../components/CheckBox";
 import Image from "../../../assets/characters/bean-2.svg";
 import Arrow from "../../../assets/graphic-devices/primary-color-arrow-1.svg";
 import { Link } from "@reach/router";
+import ProgressBar from "../../../components/ProgressBar";
 
 const PageOne = props => {
   const { masterValues, changeMaster } = props;
@@ -18,81 +19,87 @@ const PageOne = props => {
   };
 
   return (
-    <div className={styles.page}>
-      <div className={styles.box}>
-        <div className={styles.arrows}>
-          <Link to="../home-page">
-            <img
-              className={styles.leftArrow}
-              src={Arrow}
-              onClick={updateMasterValues}
-            />
-          </Link>
-          <Link to="../page-two">
-            <img
-              className={styles.rightArrow}
-              src={Arrow}
-              onClick={updateMasterValues}
-            />
-          </Link>
+    <>
+      <div className={styles.page}>
+        <div className={styles.progressBar}>
+          <ProgressBar value="25" />
         </div>
-        <section>
-          <h2>What is your name?</h2>
-          <InputLabel isPrimary={true} labelName={"First name"} />
-          <div className={styles.inputFieldContainer}>
-            <InputField
-              name={"firstName"}
-              selectInput={input =>
-                setFormValues({ ...formValues, firstName: input })
+        <div className={styles.box}>
+          <div className={styles.arrows}>
+            <Link to="../home-page">
+              <img
+                className={styles.leftArrow}
+                src={Arrow}
+                onClick={updateMasterValues}
+              />
+            </Link>
+            <Link to="../page-two">
+              <img
+                className={styles.rightArrow}
+                src={Arrow}
+                onClick={updateMasterValues}
+              />
+            </Link>
+          </div>
+
+          <section>
+            <h2>What is your name?</h2>
+            <InputLabel isPrimary={true} labelName={"First name"} />
+            <div className={styles.inputFieldContainer}>
+              <InputField
+                name={"firstName"}
+                selectInput={input =>
+                  setFormValues({ ...formValues, firstName: input })
+                }
+              />
+            </div>
+            <InputLabel isPrimary={true} labelName={"Last name"} />
+            <div className={styles.inputFieldContainer}>
+              <InputField
+                name={"secondName"}
+                selectInput={input =>
+                  setFormValues({ ...formValues, secondName: input })
+                }
+              />
+            </div>
+          </section>
+          <section>
+            <h2>What is your date of birth?</h2>
+            <InputLabel isPrimary={true} labelName={"dd/mm/yyyy"} />
+            <div className={styles.inputFieldContainer}>
+              <InputField
+                name={"dateofBirth"}
+                selectInput={input =>
+                  setFormValues({ ...formValues, dateOfBirth: input })
+                }
+              />
+            </div>
+          </section>
+          <section>
+            <h2>What is your biological gender?</h2>
+            <RadioButton
+              text="Male"
+              name="Gender"
+              value="Male"
+              startChecked={false}
+              selectRadio={input =>
+                setFormValues({ ...formValues, gender: input })
               }
             />
-          </div>
-          <InputLabel isPrimary={true} labelName={"Last name"} />
-          <div className={styles.inputFieldContainer}>
-            <InputField
-              name={"secondName"}
-              selectInput={input =>
-                setFormValues({ ...formValues, secondName: input })
+            <RadioButton
+              text="Female"
+              name="Gender"
+              value="Female"
+              startChecked={false}
+              selectRadio={input =>
+                setFormValues({ ...formValues, gender: input })
               }
             />
-          </div>
-        </section>
-        <section>
-          <h2>What is your date of birth?</h2>
-          <InputLabel isPrimary={true} labelName={"dd/mm/yyyy"} />
-          <div className={styles.inputFieldContainer}>
-            <InputField
-              name={"dateofBirth"}
-              selectInput={input =>
-                setFormValues({ ...formValues, dateOfBirth: input })
-              }
-            />
-          </div>
-        </section>
-        <section>
-          <h2>What is your biological gender?</h2>
-          <RadioButton
-            text="Male"
-            name="Gender"
-            value="Male"
-            startChecked={false}
-            selectRadio={input =>
-              setFormValues({ ...formValues, gender: input })
-            }
-          />
-          <RadioButton
-            text="Female"
-            name="Gender"
-            value="Female"
-            startChecked={false}
-            selectRadio={input =>
-              setFormValues({ ...formValues, gender: input })
-            }
-          />
-        </section>
+          </section>
+        </div>
+        <img className={styles.questionPerson} src={Image} alt="Veg" />
       </div>
-      <img className={styles.questionPerson} src={Image} alt="Veg" />
-    </div>
+    </>
   );
 };
 
