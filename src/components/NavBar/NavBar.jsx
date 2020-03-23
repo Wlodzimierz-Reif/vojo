189a8a852820c87c8eb711679d49a2da5dc46d08
Abdirisaq Omer <Abdirisaq.Omer@Nology.local>
2020-03-23 18:11:07 +0000

    Done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./NavBar.module.scss";
 import logos from "../../assets/logos/black-logo.png";
-import Button from "../Button"
+import Button from "../Button";
+import data from "../../data/index.json";
 
 const mainLinks = [
   { name: "Dashboard" },
@@ -10,62 +11,70 @@ const mainLinks = [
   { name: "Nutrient breakdown" },
   { name: "Recipes" },
   { name: "Genetics" },
-  { name: "Profile" },
+  { name: "Profile" }
 ];
 
 const getLinks = link => {
   return <p> {link.name} </p>;
 };
 
+console.log(data["user-dashboard"]);
+
 const currentPage = "Dashboard";
 
-const NavBar = (props) => {
-
+const NavBar = props => {
   // const { currentPage } = props;
   const [navVisibility, toggleOpen] = useState(false);
 
-  let switchBurgerIcon = navVisibility === true ? styles.burgerMenuCross : styles.burgerMenuIcon;
+  let switchBurgerIcon =
+    navVisibility === true ? styles.burgerMenuCross : styles.burgerMenuIcon;
 
- 
   const navOpen = {
     height: { height: "calc(100vh - 32px)" },
-    showCurrentPage: {display: "none"}
-  }
+    showCurrentPage: { display: "none" }
+  };
 
   const navClosed = {
-    height: { height: "56px", borderBottom: "solid 2px $primary-background"},
-    hide: {opacity: "0", transition: "0.2s"}
-  }
-  
+    height: { height: "56px", borderBottom: "solid 2px $primary-background" },
+    hide: { opacity: "0", transition: "0.2s" }
+  };
+
   let toggleNav = navVisibility === true ? navOpen : navClosed;
 
-  window.onresize = () => window.innerWidth >= 768 ? toggleOpen(true) : null;
+  window.onresize = () => (window.innerWidth >= 768 ? toggleOpen(true) : toggleOpen(false));
+
+  useEffect(() => {
+    if (window.innerWidth >= 768) {
+      toggleOpen(true);
+    }
+  }, []);
 
   return (
     <>
-    <div className={`${styles.navBar} `} style={toggleNav.height}>
-      <section className={styles.navTop}>
-        <div className={styles.logoBurgerFlex}>
-          <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
-          <p style={toggleNav.showCurrentPage}>{currentPage}</p>
-          <button onClick={() => toggleOpen(!navVisibility)}
-          className={`${styles.burgerMenu} ${switchBurgerIcon}`}></button>
-        </div>
-        <div className={styles.links} style={toggleNav.hide}>
-          <p className={styles.navLine}></p>
-          {mainLinks.map(getLinks)}
-          <p className={styles.navLine}></p>
-        </div>
-      </section>
-      <section className={styles.navBottom} style={toggleNav.hide}>
-        <p>Ask a question</p>
-        <p>T&Cs</p>
-        <p>Legal stuff</p>
-        <Button btnText={"Join us on Facebook"} />
-      </section>
-    </div>
+      <div className={`${styles.navBar} `} style={toggleNav.height}>
+        <section className={styles.navTop}>
+          <div className={styles.logoBurgerFlex}>
+            <img src={logos} alt="Logo" className={styles.navBarLogo}></img>
+            <p style={toggleNav.showCurrentPage}>{currentPage}</p>
+            <button
+              onClick={() => toggleOpen(!navVisibility)}
+              className={`${styles.burgerMenu} ${switchBurgerIcon}`}
+            ></button>
+          </div>
+          <div className={styles.links} style={toggleNav.hide}>
+            <p className={styles.navLine}></p>
+            {mainLinks.map(getLinks)}
+            <p className={styles.navLine}></p>
+          </div>
+        </section>
+        <section className={styles.navBottom} style={toggleNav.hide}>
+          <p>Ask a question</p>
+          <p>T&Cs</p>
+          <p>Legal stuff</p>
+          <Button btnText={"Join us on Facebook"} />
+        </section>
+      </div>
     </>
-    
   );
 };
 
