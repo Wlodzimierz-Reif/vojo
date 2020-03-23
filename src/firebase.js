28580a563109e8419afedf14c365fd07b3ee265c
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-03-23 16:36:21 +0000

    plzzzzzz travis

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_VOJO_API_KEY,
+    apiKey: `${process.env.REACT_APP_VOJO_API_KEY}`,
     authDomain: "vojo-bbe4f.firebaseapp.com",
     databaseURL: "https://vojo-bbe4f.firebaseio.com",
     projectId: "vojo-bbe4f",
