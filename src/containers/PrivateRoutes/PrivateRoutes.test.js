879d8b72b0cee54c609f0de7f78de69a1d32f7f0
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-03-25 09:48:27 +0000

    testing-fixed

--- src/containers/PrivateRoutes/PrivateRoutes.test.js
+++ src/containers/PrivateRoutes/PrivateRoutes.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PrivateRoutes from "./PrivateRoutes";
 
+import { render } from '@testing-library/react';
+
 describe("PrivateRoutes tests", () => {
     it('should render', () => {
       expect(render(<PrivateRoutes />)).toBeTruthy();
