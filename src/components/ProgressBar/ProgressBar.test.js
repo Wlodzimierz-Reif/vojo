62c197b6115b745984e5781b860fb9be17eb88bf
Wlodzimierz Reif <Wlodzimierz.Reif@Nology.local>
2020-03-25 10:12:00 +0000

    fixed test

--- src/components/ProgressBar/ProgressBar.test.js
+++ src/components/ProgressBar/ProgressBar.test.js
@@ -1,10 +1,10 @@
 import React from "react";
+import { render } from "@testing-library/react";
 import ProgressBar from "./ProgressBar";
 
 describe("ProgressBar tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
+  const testVal = 50;
+  it("should render", () => {
+    expect(render(<ProgressBar value={testVal} />)).toBeTruthy();
   })
-});
+})
