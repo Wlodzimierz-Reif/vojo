45cc44dc94fb5468c8d8e522c1aa83b42d728bc1
Your Name <jjgray90@gmail.com>
2020-03-25 16:58:29 +0000

    fixed test

--- src/containers/LandingPage/LandingPage.test.js
+++ src/containers/LandingPage/LandingPage.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 import LandingPage from "./LandingPage";
+import { render } from "@testing-library/react";
 
 describe("LandingPage tests", () => {
-  let component;
-
-  beforeEach(() => {
-    component =
-  })
+  it("should render", () => {
+    expect(render(<LandingPage />)).toBeTruthy();
+  });
 });
