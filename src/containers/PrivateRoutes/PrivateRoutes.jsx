879486a786ce70b4347576b76ed90092a2720924
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-03-25 09:52:22 +0000

    test

--- src/containers/PrivateRoutes/PrivateRoutes.jsx
+++ src/containers/PrivateRoutes/PrivateRoutes.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import styles from "./PrivateRoutes.module.scss";
 import { redirectTo } from "@reach/router";
 import Footer from "../../components/Footer";
 
