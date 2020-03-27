e373bceff28a7a8a7652918c84972b8e35defac2
Bogdan Niculescu <Bogdan.Niculescu@Nology.local>
2020-03-27 16:28:13 +0000

    addded image and styles to everydayfoods

--- src/containers/EverydayFoods/EverydayFoods.jsx
+++ src/containers/EverydayFoods/EverydayFoods.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "../EverydayFoods/EverydayFoods.module.scss";
 import InfoList from "../../components/InfoList";
-import { firestore } from "../../firebase";
+import Image from "../../assets/characters/pickle-2.svg";
 
 const EverydayFoods = props => {
   const { everydayFoods } = props;
@@ -11,6 +11,7 @@ const EverydayFoods = props => {
       <InfoList
         infoHeaderBlack={item.name}
         infoHeaderPurple={item.recommendation}
+        foodList={item["intolerance-action"]}
       />
     ));
   };
@@ -20,39 +21,7 @@ const EverydayFoods = props => {
       <section className={styles.dietContainer}>
         <h3 className={styles.title}>Your go-to everyday foods</h3>
         {getItemJsx(everydayFoods)}
-        {/* <InfoList
-            infoHeaderBlack={user.uid["food-stuffs"].name}
-            infoHeaderPurple={"Coconut or Soya"}
-            foodList={
-              "Plant-based milks and yoghurts are a good alternative to dairy foods and can be a good source of vitamins and minerals, such as calcium, as they are often fortified. As you handle fats better than carbohydrates, go for coconut or soya milk as these are higher in fat and lower in carbohydrate." */}
-        {/* }
-          />
-        {/* <InfoList
-            infoHeaderBlack={"Other Vegan Dairy Alternatives"}
-            foodList={
-              "There is an increasing variety of plant-based cheeses and spreads, but these are highly processed foods, often high in fat and salt, so should only be eaten as a occasionally."
-            }
-          />
-          <InfoList
-            infoHeaderBlack={"Your cooking oil: "}
-            infoHeaderPurple={"Coconut or olive"}
-            foodList={
-              "Your Cooking Oil: Coconut or Olive Some fat in the diet is essential to absorb nutrients. Choose fats from whole foods like flaxseeds, nuts, and avocados as opposed to refined, liquid oils where possible. If using oil, it's best for you to use coconut oil or olive oil for cooking."
-            }
-          />
-          <InfoList
-            infoHeaderBlack={"Your Go-To Breakfast: "}
-            infoHeaderPurple={"Tofu Scramble with Spinach & Avocado"}
-            foodList={
-              "Your Go-To Breakfast: Tofu Scramble with Spinach & Avocado Ditch the porridge and toast in the mornings and go for a healthy combo of fats and protein instead. It'll be better for your blood sugar!"
-            }
-          />
-          <InfoList
-            infoHeaderBlack={"Your Guilty Pleasure: "}
-            infoHeaderPurple={"Dark Chocolate"}
-            foodList={
-              "We all have that food... and yours is a good dose of dark chocolate now and again!"
-            } */}
+        <img src={Image} alt="" />
       </section>
     </div>
   );
