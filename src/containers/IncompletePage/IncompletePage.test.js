a7f0b34646c428ff9a55da5f3b36c8f262d1beb0
Charlie Caseley <Charlie.Caseley@Nology.local>
2020-03-30 15:20:28 +0100

    wokrd on tests

--- src/containers/IncompletePage/IncompletePage.test.js
+++ src/containers/IncompletePage/IncompletePage.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import IncompletePage from "./IncompletePage";
+import { render } from "@testing-library/react";
 
 describe("IncompletePage tests", () => {
   it("should render", () => {
