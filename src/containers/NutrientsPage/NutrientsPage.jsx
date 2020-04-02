1a83f369e95fb6029515903edf20f6e71400bfaf
Your Name <jjgray90@gmail.com>
2020-04-02 14:36:37 +0100

    changed h1 to h2

--- src/containers/NutrientsPage/NutrientsPage.jsx
+++ src/containers/NutrientsPage/NutrientsPage.jsx
@@ -64,7 +64,7 @@ const NutrientsPage = props => {
       <section className={styles.nutrientsFlex}>
         <section className={styles.showPanel}>{insertJsx}</section>
         <section className={styles.nutrientPage}>
-          <h1>Nutrients</h1>
+          <h2>Nutrients</h2>
           <div className={styles.infoCardHolder}>
             {mapFunction(high)}
             {mapFunction(slightlyRaised)}
